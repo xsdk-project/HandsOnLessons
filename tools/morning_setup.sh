d3d6e0e2c5a9702b980a445b55316b1d27c35288
miller86 <miller86@llnl.gov>
2017-08-05 09:47:28 -0700

    updating vnc startup tool

--- tools/morning_setup.sh
+++ tools/morning_setup.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/bin/sh -x
+
+acct=ATPESC17_Instructors
+cooley_username=mcmiller
+nnodes=1
+tl=5
+cooley_shell=tcsh
+localos=osx
 
 # Ensure ~/.ssh/config exists and has limited permissions
 if [[ ! -e ~/.ssh/config ]]; then
@@ -31,30 +38,44 @@ ssh cooley "cat > ~/.vnc/xstartup" << EOF
 xterm &
 twm
 EOF
-ssh cooley "chmod u+x ~/.vnc/xstartup
+ssh cooley "chmod u+x ~/.vnc/xstartup"
 
 while true; do
     read -p "Create temporary VNC Password: " pw
     echo "You have entered \"$pw\", is this correct?"
     select yn in "Yes" "No"; do
         case $yn in
-            Yes ) exit;;
-            No ) break;;
+            Yes ) break 2;;
         esac
     done
 done
-ssh cooley "cat > ~/.vnc/password" << EOF
+ssh cooley "rm -f ~/.vnc/password; cat > ~/.vnc/password" << EOF
 $pw
 EOF
 
 # Reserve 3 nodes for interactive use all day
 # get back allocation machine 'name' and start
 # xvnc server there
-ssh cooley "qsub -I -n 1 -t 5 -A ATPESC17_Instructors" 
+qsub_job_info=$(ssh -f cooley "qsub -I -n $nnodes -t $tl -A $acct")
+nodid=$(echo $qsub_job_info | tr ' ' '\n' | grep cc[0-9][0-9][0-9].cooley)
+
+#
+# Startup xvncserver on the allocation
+#
+ssh cooley "ssh $nodid x0vncserver --display=:0.0 --NeverShared=1 --geometry=2400x1500+0+0 --PasswordFile=/home/$cooley_username/.vnc/passwd --MaxProcessorUsage=100"
 
 # Set up 2-hop ssh tunnel to allocation, (above) through
 # login and run xstartup there
+if [[ "$cooley_shell" == tcsh ]]; then
+    ssh -f -L 22590:$nodid:5900 cooley "ssh $nodid 'setenv DISPLAY :0.0; ~/.vnc/xstartup'"
+else
+    ssh -f -L 22590:$nodid:5900 cooley "ssh $nodid 'export DISPLAY=:0.0; ~/.vnc/xstartup'"
+fi
 
-# OSX: open vnc://localhost:22590
-# Windows:
-# Linux
+if [[ "$localos" == osx ]]; then
+    open vnc://localhost:22590 &
+elif [[ "$localos" == linux ]]; then
+    echo "not implemented"
+elif [[ "$localos" == windows ]]; then
+    echo "not implemented"
+fi
